964b032bbdc227876fde408f9632abba33f85dcf
zhuxuetong123 <zhuxuetong@telchina.net>
2019-05-16 17:06:18 +0800

    1、popupBackground属性处理

--- libSkin/src/main/java/com/telchina/libskin/attr/PopupBackgroundAttr.java
+++ libSkin/src/main/java/com/telchina/libskin/attr/PopupBackgroundAttr.java
@@ -1,11 +1,14 @@
 package com.telchina.libskin.attr;
 
+import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
+import android.support.v7.widget.CardView;
 import android.view.View;
 import android.widget.Spinner;
 
 import com.telchina.libskin.load.SkinManager;
+import com.telchina.libskin.util.L;
 
 /**
  * src属性替换
@@ -15,9 +18,18 @@ public class PopupBackgroundAttr extends SkinAttr {
     @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     public void apply(View view) {
-        if (view instanceof Spinner) {
+        if (RES_TYPE_NAME_COLOR.equals(attrValueTypeName)) {
+
+            int color = SkinManager.getInstance().getColor(attrValueRefId);
+            if (view instanceof Spinner) {
+                Spinner spinner = (Spinner) view;
+                spinner.setPopupBackgroundResource(SkinManager.getInstance().getColor(attrValueRefId));
+            }
+        } else if (RES_TYPE_NAME_DRAWABLE.equals(attrValueTypeName)) {
+            Drawable bg = SkinManager.getInstance().getDrawable(attrValueRefId);
             Spinner spinner = (Spinner) view;
-            spinner.setPopupBackgroundResource(SkinManager.getInstance().getColor(attrValueRefId));
+            spinner.setPopupBackgroundDrawable(bg);
         }
+
     }
 }
